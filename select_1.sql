5b78cd2d4db209107a532bf16e956f0438e649f7
Paulo César dos Passos Pereira <paulo.passos1@gmail.com>
2024-07-22 11:07:04 -0300

    comando select simples 1

--- select_1.sql
+++ select_1.sql
@@ -3,3 +3,7 @@ SELECT * FROM cliente;
 SELECT nome, data_nascimento FROM cliente;
 
 SELECT nome, data_nascimento as "Data de nascimento" FROM cliente;
+
+SELECT 'CPF: ' || cpf || ' RG: ' || rg as "CPF e RG" FROM cliente;
+
+SELECT * FROM cliente LIMIT 3;
